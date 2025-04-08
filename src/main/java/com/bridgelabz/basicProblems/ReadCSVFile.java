b26c31db82885f053e707da24ff1478f09894a90
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-08 11:22:17 +0530

    [Sanya] Add: Read a CSV file and print data

---
+++ src/main/java/com/bridgelabz/basicProblems/ReadCSVFile.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.basicProblems;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadCSVFile {
+    public static void main(String[] args) {
+        String path="/Users/sanyakhanna/Desktop/bridgelabz-workspace/java-csv-handling/src/main/java/com/bridgelabz/basicProblems/students.csv";
+        try(BufferedReader br=new BufferedReader(new FileReader(path))) {
+            String line;
+            while((line=br.readLine())!=null){
+                String[] col=line.split(",");
+                System.out.println("ID: "+col[0]+", Name: "+col[1]+", Age: "+col[2]+", Marks: "+col[3]);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
