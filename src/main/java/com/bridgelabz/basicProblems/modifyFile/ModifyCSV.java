db700e2d75ae2cc25cd4e4f7bfb7bcf52c4140ae
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-08 20:41:09 +0530

    [Sanya] Add: Modify a record in CSV file

---
+++ src/main/java/com/bridgelabz/basicProblems/modifyFile/ModifyCSV.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.basicProblems.modifyFile;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.*;
+import java.io.*;
+import java.util.List;
+
+public class ModifyCSV {
+    public static void main(String[] args) {
+        try(CSVReader reader = new CSVReader(new FileReader("/Users/sanyakhanna/Desktop/bridgelabz-workspace/java-csv-handling/src/main/java/com/bridgelabz/basicProblems/writeCSV/employee.csv"));
+            CSVWriter writer = new CSVWriter(new FileWriter("/Users/sanyakhanna/Desktop/bridgelabz-workspace/java-csv-handling/src/main/java/com/bridgelabz/basicProblems/modifyFile/updated_employee.csv"))){
+            String[] header;
+            header = reader.readNext();
+            writer.writeNext(header);
+            String[] read;
+            while((read = reader.readNext())!=null){
+                if(read[2].equals("IT")){
+                    double salary = Double.parseDouble(read[3]);
+                    salary = salary*1.10;
+                    read[3] = String.valueOf(salary);
+                }
+                writer.writeNext(read);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
